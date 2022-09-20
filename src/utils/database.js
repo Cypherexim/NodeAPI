2994f874812801a6de8569ed6c4f143c7ee28fca
Sumit <sumitpandit1191@gmail.com>
2022-09-21 00:38:25 +0530

    DB credentials moved to env file

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,19 +1,20 @@
 const Pool = require('pg').Pool;
+const env = require('dotenv').config()
 
 // const pool = new Pool({
-//     user:'postgres',
-//     host: 'localhost',
-//     database:'TestDB',
-//     password: 'admin',
+//     user:'eximread',
+//     host: '18.214.151.123',
+//     database:'exim_portal_db',
+//     password: 'readonly@123',
 //     port:5432
 // })
 
 const pool = new Pool({
-    user:'eximread',
-    host: '18.214.151.123',
-    database:'exim_portal_db',
-    password: 'readonly@123',
-    port:5432
+    user:process.env.USER,
+    host: process.env.HOST,
+    database:process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port:process.env.PORT
 })
 
 module.exports = pool;
