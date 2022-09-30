cfde91299f5e5ab63342c8a5eb25a9af57b4a538
Sumit <sumitpandit1191@gmail.com>
2022-09-30 22:19:51 +0530

    add cors issue

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended:true}));
-// app.use((req,res,next) =>{
-//     res.setHeader('Access-Control-Allow-Origin','*');
-//     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-//     res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
-//     next();
-// })
-app.use(cors());
+app.use((req,res,next) =>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+    next();
+})
+//app.use(cors());
 app.use('/user', routes);
 app.listen(8080);
