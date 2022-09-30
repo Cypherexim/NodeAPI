a160cb06681ee0130d3787bfab9200272446920c
Sumit <sumitpandit1191@gmail.com>
2022-09-30 17:56:56 +0530

    Resolved COrs issue

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const routes = require('./src/routes/routes');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.urlencoded({extended:true}));
+app.use((req,res,next) =>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET','POST','PUT','PATCH','DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+    next();
+})
 app.use('/user', routes);
 app.listen(8080);
