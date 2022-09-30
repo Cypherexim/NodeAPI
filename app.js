6a7561660d23c184c800a460d0fd0caccbaea431
Sumit <sumitpandit1191@gmail.com>
2022-10-01 00:12:32 +0530

    cors enabled

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./src/routes/routes');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended:true}));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
-// app.use(cors());
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
+ app.use(cors());
 app.use('/user', routes);
 app.listen(8080);
