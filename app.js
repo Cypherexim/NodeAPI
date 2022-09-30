4af29da66b699c7ed2a04b1df3b036fe1194db0e
Sumit <sumitpandit1191@gmail.com>
2022-10-01 00:09:50 +0530

    cors issue fix

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended:true}));
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 // app.use(cors());
