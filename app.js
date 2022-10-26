0d8d12448f7bf427b2e98fe6efd2db39958d27c5
Sumit <sumitpandit1191@gmail.com>
2022-10-26 21:52:39 +0530

    Fixed cors issue

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const routes = require('./src/routes/routes');
 const cors = require('cors');
 
 const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
-app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+//app.use(cors());
 app.use('/user', routes);
 app.listen(8080);
