f5e6093a29f131349d855794246ccb69769b4902
sumitpndt <sumitpandit1191@gmail.com>
2023-01-08 14:42:01 +0530

    change code

--- src/controllers/Export/philipController.js
+++ src/controllers/Export/philipController.js
@@ -11,12 +11,18 @@ const common = require('../../utils/common');
 exports.getphilipExport = async (req, res) => {
     //db.connect();
     try {
-        const { fromDate, toDate, HSCODE, HSCodeDesc, Importer_Name, EXPORTER_NAME,UserId,IsWorkspaceSearch= false ,page, itemperpage } = req.query;
-        const check = await common.deductSearches(UserId,IsWorkspaceSearch);
+        const { fromDate, toDate, HSCODE, HSCodeDesc, Importer_Name, EXPORTER_NAME, UserId, IsWorkspaceSearch = false, page, itemperpage } = req.query;
+        const check = await common.deductSearches(UserId, IsWorkspaceSearch);
         if (check) {
-            await db.query(query.get_philip_export, [fromDate, toDate, HSCODE, HSCodeDesc, Importer_Name, EXPORTER_NAME,parseInt(itemperpage),(parseInt(page) - 1) * parseInt(itemperpage)], (error, results) => {
-                return res.status(200).json(success("Ok", results.rows, res.statusCode));
-            })
+            if (page != null && itemperpage != null) {
+                db.query(query.get_philip_export_pagination, [fromDate, toDate, HSCODE, HSCodeDesc, Importer_Name, EXPORTER_NAME, parseInt(itemperpage), (parseInt(page) - 1) * parseInt(itemperpage)], (error, results) => {
+                    return res.status(200).json(success("Ok", results.rows, res.statusCode));
+                })
+            } else {
+                db.query(query.get_philip_export, [fromDate, toDate, HSCODE, HSCodeDesc, Importer_Name, EXPORTER_NAME], (error, results) => {
+                    return res.status(200).json(success("Ok", results.rows, res.statusCode));
+                })
+            }
         } else {
             return res.status(200).json(error("You don't have enough search credit please contact admin to recharge !"));
         }
