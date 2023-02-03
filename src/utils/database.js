c811d48bfa7cf5d2f1d2de2ac0b063bfc4355797
sumitpndt <sumitpandit1191@gmail.com>
2023-02-03 13:54:45 +0530

    commit changes

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,11 +1,21 @@
 const Pool = require('pg').Pool;
 const env = require('dotenv').config()
 
+// Development - Cypher
+// const pool = new Pool({
+//     user:'cypher',
+//     host: '18.214.151.123',
+//     database:'cypher',
+//     password: 'cypher123',
+//     port:5432
+// })
+
+// Production - Exim
 const pool = new Pool({
-    user:'cypher',
+    user:'apiuser',
     host: '18.214.151.123',
-    database:'cypher',
-    password: 'cypher123',
+    database:'exim_portal_db',
+    password: 'apiuser123',
     port:5432
 })
 
