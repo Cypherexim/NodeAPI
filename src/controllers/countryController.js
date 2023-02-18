d8fdffb5156e9c23c6345a0b91c961195ac5e821
sumitpndt <sumitpandit1191@gmail.com>
2023-02-18 23:52:47 +0530

    commit changes

--- src/controllers/countryController.js
+++ src/controllers/countryController.js
@@ -16,3 +16,20 @@ exports.getCountries = async (req, res) => {
     };
     //db.end;
 }
+
+exports.addCountry = async (req, res) => {
+    const { countryCode, countryName, imp, exp } = req.body;
+    try {
+        db.query(query.addCountry, [countryCode, countryName, imp, exp], (error, results) => {
+            if (!error) {
+                db.query(query.addDownloadCost, [countryCode, 1], (err, results) => {
+                    if (!err) {
+                        return res.status(200).json(success("Ok", results.rows, res.statusCode));
+                    }
+                })
+            }
+        })
+    } catch (err) {
+        return res.status(500).json(error(err, res.statusCode));
+    };
+}
