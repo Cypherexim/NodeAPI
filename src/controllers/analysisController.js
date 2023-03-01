b32f262d1a93313e8676ec40c6fc75dc98f76f00
sumitpndt <sumitpandit1191@gmail.com>
2023-03-01 20:14:09 +0530

    commit changes

--- src/controllers/analysisController.js
+++ src/controllers/analysisController.js
@@ -16,33 +16,37 @@ exports.getAnalysisData = async (req, res) => {
             NotifyPartyName, countryname, direction, fieldName = 'HsCode' } = req.body;
         const Requestedfield = [fieldName];
         const requestedfieldavailable = await db.query('SELECT column_name FROM information_schema.columns WHERE table_name = $1 and column_name = ANY($2)', [direction.toLowerCase() + '_' + countryname.toLowerCase(), Requestedfield]);
-        if(requestedfieldavailable.rows.length >0){
-        const fieldList = ["Quantity", "ValueInUSD", "UnitPriceUSD"];
-        const availablefield = await db.query('SELECT column_name FROM information_schema.columns WHERE table_name = $1 and column_name = ANY($2)', [direction.toLowerCase() + '_' + countryname.toLowerCase(), fieldList]);
-        if (availablefield.rows.length > 0) {
-            var fields = [];
-            availablefield.rows.forEach(x => {
-                if (x.column_name.toString() != "UnitPriceUSD") {
-                    fields.push('ROUND(SUM("' + x.column_name.toString() + '")::numeric,2) as ' + x.column_name.toString());
-                } else {
-                    fields.push('ROUND(AVG("' + x.column_name.toString() + '")::numeric,2) as ' + x.column_name.toString());
-                }
-            })
-            const query = '"' + fieldName + '", ' + fields.join(",") + ' FROM '; // +  + ' GROUP BY "HsCode"';
-            const finalquery = await common.getExportData(fromDate, toDate, HsCode, ProductDesc, Imp_Name, Exp_Name, CountryofOrigin,
-                CountryofDestination, Month, Year, uqc, Quantity, PortofOrigin,
-                PortofDestination,
-                Mode, LoadingPort,
-                NotifyPartyName, Currency, 0, 0, query, direction.toLowerCase() + '_' + countryname.toLowerCase(), false);
-            db.query(finalquery[0] + ' GROUP BY "' + fieldName + '"', finalquery[1].slice(1), (err, result) => {
-                return res.status(200).json(success("Ok", result.rows, res.statusCode));
-            });
+        if (requestedfieldavailable.rows.length > 0) {
+            const fieldList = ["Quantity", "ValueInUSD", "UnitPriceUSD"];
+            const availablefield = await db.query('SELECT column_name FROM information_schema.columns WHERE table_name = $1 and column_name = ANY($2)', [direction.toLowerCase() + '_' + countryname.toLowerCase(), fieldList]);
+            if (availablefield.rows.length > 0) {
+                var fields = [];
+                availablefield.rows.forEach(x => {
+                    if (x.column_name.toString() != "UnitPriceUSD") {
+                        fields.push('ROUND(SUM("' + x.column_name.toString() + '")::numeric,2) as ' + x.column_name.toString());
+                    } else {
+                        fields.push('ROUND(AVG("' + x.column_name.toString() + '")::numeric,2) as ' + x.column_name.toString());
+                    }
+                })
+                const query = '"' + fieldName + '", ' + fields.join(",") + ' FROM '; // +  + ' GROUP BY "HsCode"';
+                const finalquery = await common.getExportData(fromDate, toDate, HsCode, ProductDesc, Imp_Name, Exp_Name, CountryofOrigin,
+                    CountryofDestination, Month, Year, uqc, Quantity, PortofOrigin,
+                    PortofDestination,
+                    Mode, LoadingPort,
+                    NotifyPartyName, Currency, 0, 0, query, direction.toLowerCase() + '_' + countryname.toLowerCase(), false);
+                db.query(finalquery[0] + ' GROUP BY "' + fieldName + '"', finalquery[1].slice(1), (err, result) => {
+                    if (!err) {
+                        return res.status(200).json(success("Ok", result.rows, res.statusCode));
+                    } else {
+                        return res.status(200).json(success("Ok", err.message, res.statusCode));
+                    }
+                });
+            } else {
+                return res.status(200).json(success("Ok", "Seems either of column Quantity/ValueInUSD/UnitPriceUSD not available in table.", res.statusCode));
+            }
         } else {
-            return res.status(200).json(success("Ok", "Seems either of column Quantity/ValueInUSD/UnitPriceUSD not available in table.", res.statusCode));
+            return res.status(200).json(success("Ok", "Seems column " + fieldName + " not available in table so can't produce analysis.", res.statusCode));
         }
-    }else {
-        return res.status(200).json(success("Ok", "Seems column "+fieldName+" not available in table so can't produce analysis.", res.statusCode));
-    }
     } catch (err) {
         return res.status(500).json(error(err, res.statusCode));
     };
