c9d15cda39a668b9ea1c1f80983b2101fea2c8d7
sumitpndt <sumitpandit1191@gmail.com>
2023-09-01 22:41:41 +0530

    commit cors

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.use((req, res, next) => {
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-//app.use(cors());
+app.use(cors());
 app.use('/api', routes);
 app.listen(8080);
